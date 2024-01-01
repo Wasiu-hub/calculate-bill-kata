b540c14b3ac979baa83df5ca7ce92720785c9fcc
Wasiu-hub <contactbow@yahoo.com>
2024-01-01 05:38:51 +0000

    returning the right amount test passed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const calculateBill = (price, vat, tip) => {
   const amount = price + (price * vat) / 100 + tip;
-  return `${amount}`;
-  //   return amount.toString();
+  return `£${amount}`;
+  //   return "£" + amount.toString();
 };
 
 module.exports = calculateBill;
