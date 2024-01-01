21b7e1794d9a86bcd7c03a58a225b6615991978a
Wasiu-hub <contactbow@yahoo.com>
2024-01-01 05:26:25 +0000

    returning a string test passed

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const calculateBill = (price, vat, tip) => {};
+const calculateBill = (price, vat, tip) => {
+  const amount = price + (price * vat) / 100 + tip;
+  return `${amount}`;
+  //   return amount.toString();
+};
 
 module.exports = calculateBill;
+
+console.log(calculateBill(100, 2, 10));
